a2dd5a1330bd753eccd662727df8f8b6beb46077
Ahmed <ahmed@hayasolutions.com>
2020-04-03 17:57:22 +0200

    added appbar component

---
+++ src/Components/Homepage/AppBar/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
+
+const AppBar = (props) => {
+  const { loggedIn, userData, setLoggedIn } = props;
+
+  return (
+    <div className="app-bar">
+      <div className="title-container">
+        <h1 className="title">Guestbook</h1>
+      </div>
+      <div className="account-container">
+        {loggedIn ? (
+          <div class="dropdown">
+            <span>{userData.name}</span>
+            <div class="dropdown-content">
+              <button
+                className="drop-down-button"
+                onClick={() => {
+                  setLoggedIn(false);
+                }}
+              >
+                Log out
+              </button>
+            </div>
+          </div>
+        ) : (
+          <Link to="/sign-in">
+            <button className="sign-in-button">Sign in</button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AppBar;

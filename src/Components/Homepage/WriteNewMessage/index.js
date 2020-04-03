b9225c266a3310c089016c658e0a24c3b8124ff9
Ahmed <ahmed@hayasolutions.com>
2020-04-03 18:54:51 +0200

    added text area and button to the new message component

--- src/Components/Homepage/WriteNewMessage/index.js
+++ src/Components/Homepage/WriteNewMessage/index.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
 const WriteNewMessage = (props) => {
-  return <div></div>;
+  const [messageContent, setMessageContent] = useState("");
+  const { loggedIn } = props;
+
+  return loggedIn ? (
+    <div className="write-new-message">
+      <textarea
+        value={messageContent}
+        onChange={(event) => setMessageContent(event.target.value)}
+        wrap="off"
+        className="new-message-textarea"
+        rows={5}
+        cols={20}
+      />
+      <div className="button-container">
+        <button className="post-message-button">Post Message</button>
+      </div>
+    </div>
+  ) : (
+    <p>You must be logged in to be able to post messages </p>
+  );
 };
 export default WriteNewMessage;

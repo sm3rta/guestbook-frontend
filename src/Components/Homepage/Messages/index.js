052fc055d949d398f8a9a3ed1804f65a49b577da
Ahmed <ahmed@hayasolutions.com>
2020-04-03 19:37:03 +0200

    messages component now fetches data from API

--- src/Components/Homepage/Messages/index.js
+++ src/Components/Homepage/Messages/index.js
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+console.log("process.env", process.env);
 
 const Messages = (props) => {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const getMessages = useCallback(() => {
+    setError(false);
+    setLoading(true);
+    axios
+      .get(apiEndpoint + "messages")
+      .then((res) => {
+        console.log("res", res);
+        setError(false);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    getMessages();
+  }, [getMessages]);
+
+  return !loading ? (
+    !error ? (
+      // finished loading and fetched data successfully
+      <div />
+    ) : (
+      // finished loading data but an error occurred
+      <div />
+    )
+  ) : (
+    // loading
+    <div />
+  );
 };
 export default Messages;

f611196d363d3aec588401056923c3836c2e0998
Ahmed <ahmed@hayasolutions.com>
2020-04-03 23:43:27 +0200

    posting new messages now work as expected

--- src/Components/Homepage/WriteNewMessage/index.js
+++ src/Components/Homepage/WriteNewMessage/index.js
@@ -1,26 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./index.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
 
 const WriteNewMessage = (props) => {
   const [messageContent, setMessageContent] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const history = useHistory();
   const { loggedIn, userData } = props;
+
+  const submitMessage = useCallback(() => {
+    setLoading(true);
+    setError("");
+    if (messageContent.length === 0) {
+      setError("You cannot leave the message empty!");
+      setLoading(false);
+      return;
+    }
+    if (messageContent.length < 3) {
+      setError("Please type 3 letters or more");
+      setLoading(false);
+      return;
+    }
+    axios
+      .post(apiEndpoint + "messages", {
+        content: messageContent,
+        submittedBy: userData._id,
+      })
+      .then((res) => {
+        console.log("post message res", res);
+        setError(false);
+        setMessageContent("");
+      })
+      .catch((err) => {
+        console.log("post message err", err.response.data);
+        setError(err.response.data.message);
+      })
+      .finally(() => {
+        setLoading(false);
+        history.push("/?refresh=true");
+      });
+  }, [messageContent, userData._id, history]);
 
   return (
     <div className="write-new-message">
       <textarea
         value={messageContent}
-        onChange={(event) => setMessageContent(event.target.value)}
+        onChange={(event) => {
+          setError("");
+          setMessageContent(event.target.value);
+        }}
         className="new-message-textarea"
         rows={5}
         cols={20}
         placeholder={
           loggedIn ? "Write a message" : "You must log in to post messages"
         }
-        disabled={!loggedIn}
+        disabled={!loggedIn || loading}
       />
       {loggedIn && (
         <div className="button-container">
-          <button className="post-message-button">Post Message</button>
+          {<p className="error-text">{error}</p>}
+          <button onClick={submitMessage} className="post-message-button">
+            Post Message
+          </button>
         </div>
       )}
     </div>

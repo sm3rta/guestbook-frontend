b5c47dda59fb925956a0e90832a47119feb6f9f8
Ahmed <ahmed@hayasolutions.com>
2020-04-03 17:03:33 +0200

    replaced react router with browser router

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Router, Switch, Route } from "react-router-dom";
-import AppBar from "./AppBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AppBar from "./Components/AppBar";
 
 const AppRouter = () => {
   return (

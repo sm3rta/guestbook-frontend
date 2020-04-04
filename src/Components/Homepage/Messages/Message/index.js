1f199bc154bf4588d0cdcbb8b349b9655f3437de
Ahmed <ahmed@hayasolutions.com>
2020-04-05 00:42:11 +0200

    added documentation to Message

--- src/Components/Homepage/Messages/Message/index.js
+++ src/Components/Homepage/Messages/Message/index.js
@@ -1,10 +1,61 @@
+/**
+ * @module Message
+ */
 import React, { useState, useCallback, useEffect } from "react";
 import axios from "axios";
 import "./index.css";
 import CircularLoading from "../../../../common/CircularLoading";
 import moment from "moment";
+import PropTypes from "prop-types";
 
 const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+
+/**
+ * @method Message
+ * @see Messages
+ * @requires CircularLoading
+ * @desc Shows a card with message info, replies to it and a field to add a reply
+ * @param {Object} props Component props
+ * @param {Object}  props.userData an object containing user data
+ * @param {string}  props.userData._id currently logged in user ID
+ * @param {string}  props.userData.name currently logged in user's name
+ * @param {Object} props.message message object
+ * @param {string} props.message._id message ID
+ * @param {string} props.message.content message text
+ * @param {Object} props.message.submittedBy object containing info about the message submitter
+ * @param {string} props.message.submittedBy._id message submitter ID
+ * @param {string} props.message.submittedBy.name message submitter name
+ * @param {Date} props.message.date date the message was submitted
+ * @param {Array.<Object>} props.message.replies replies to the message
+ * @param {string} props.message.replies.content reply text
+ * @param {Object} props.message.replies.submittedBy object containing info about the reply submitter
+ * @param {string} props.message.replies.submittedBy.name reply submitter name
+ * @param {Date} props.message.replies.date reply submission date
+ * @param {boolean} props.loggedIn whether there is a user currently logged in or not
+ * @param {function} props.getMessages a function that tells the parent component to refresh messages
+ * @example
+ *
+ * const message = {
+ *   replies: [{ content, submittedBy: { name } }],
+ *   submittedBy: { name, _id },
+ *   date,
+ *   _id,
+ *   content,
+ * };
+ * const loggedIn = true;
+ * const getMessages = function () {};
+ * const userData = {name, _id}
+ *
+ * return (
+ *   <Message
+ *     message={message}
+ *     userData={userData}
+ *     loggedIn={loggedIn}
+ *     getMessages={getMessages}
+ *   />
+ * );
+ *
+ */
 
 const Message = (props) => {
   const [edit, setEdit] = useState(false);
@@ -21,6 +72,13 @@ const Message = (props) => {
     setEditedValue(message.content);
   }, [message.content]);
 
+  /**
+   * @method
+   * @name deleteMessage
+   * @inner
+   * @public
+   * @memberof Message
+   */
   const deleteMessage = useCallback(() => {
     const messageId = message._id;
 
@@ -173,4 +231,12 @@ const Message = (props) => {
     <CircularLoading size={40} />
   );
 };
+
+Message.propTypes = {
+  userData: PropTypes.object.isRequired,
+  message: PropTypes.object.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
+  getMessages: PropTypes.func.isRequired,
+};
+
 export default Message;

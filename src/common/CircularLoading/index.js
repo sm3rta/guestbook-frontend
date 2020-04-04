c0d967dc60f72bce13ff74c4098029d07bd455c3
Ahmed <ahmed@hayasolutions.com>
2020-04-05 00:42:23 +0200

    added documentation to CircularLoading

--- src/common/CircularLoading/index.js
+++ src/common/CircularLoading/index.js
@@ -1,10 +1,32 @@
+/**
+ * @module CircularLoading
+ */
 import React from "react";
 import "./index.css";
+import PropTypes from "prop-types";
 
+/**
+ * method
+ * @desc shows a rotating loading icon
+ *
+ * @param {Object} props  Component props
+ * @param {number} props.size loader width and height in pixels
+ *
+ * @see Signin
+ * @see Signup
+ * @see Messages
+ * @see Message
+ *
+ */
 const CircularLoading = (props) => {
   const { size } = props;
   return (
     <div style={{ width: size, height: size }} className="circular-loading" />
   );
 };
+
+CircularLoading.propTypes = {
+  size: PropTypes.number.isRequired,
+};
+
 export default CircularLoading;

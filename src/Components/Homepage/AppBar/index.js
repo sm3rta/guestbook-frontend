511ddd11808ef9a284b18420be90123c9e52d62f
Ahmed <ahmed@hayasolutions.com>
2020-04-05 00:42:02 +0200

    added documentation to AppBar

--- src/Components/Homepage/AppBar/index.js
+++ src/Components/Homepage/AppBar/index.js
@@ -1,6 +1,28 @@
+/**
+ * @module AppBar
+ */
 import React from "react";
 import "./index.css";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+/**
+ * @method AppBar
+ * @desc  Component for displaying the appBar, it displays the user's name or a button to sign in
+ * @param {Object} props Component props
+ * @param {boolean} props.loggedIn whether there is a user currently logged in or not
+ * @param {Object}  props.userData an object containing user data
+ * @param {string}  props.userData._id currently logged in user ID
+ * @param {string}  props.userData.name currently logged in user's name
+ * @param {function} props.setLoggedIn a function to set loggedIn status
+ * to false when user logs out
+ *
+ * @see Homepage
+ *
+ * @example
+ * return <AppBar loggedIn={false} userData={{}} setLoggedIn={function () {}} />;
+ *
+ */
 
 const AppBar = (props) => {
   const { loggedIn, userData, setLoggedIn } = props;
@@ -33,6 +55,12 @@ const AppBar = (props) => {
       </div>
     </div>
   );
+};
+
+AppBar.propTypes = {
+  loggedIn: PropTypes.bool.isRequired,
+  userData: PropTypes.object.isRequired,
+  setLoggedIn: PropTypes.func.isRequired,
 };
 
 export default AppBar;
